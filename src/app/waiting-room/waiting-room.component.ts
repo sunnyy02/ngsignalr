71d1731068856a54a0367ed9facae312a55e6ed0
yan sun <yan.sun@humanservices.gov.au>
2020-03-24 13:33:54 +1000

    init signalr

--- src/app/waiting-room/waiting-room.component.ts
+++ src/app/waiting-room/waiting-room.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HubConnection } from '@aspnet/signalr';
+import * as signalR from '@aspnet/signalr';
 
 @Component({
   selector: 'app-waiting-room',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./waiting-room.component.scss']
 })
 export class WaitingRoomComponent implements OnInit {
-
+  private hubConnection: HubConnection;
+//https://stackoverflow.com/questions/54297637/how-to-hook-up-signalr-with-an-angular-7-application
   constructor() { }
+  public ngOnInit() {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl("http://localhost:50930/pushNotification").build();
 
-  ngOnInit() {
+    this.hubConnection.start().then(() => {
+      console.log("connection started");
+    }).catch(err => console.log(err));
+
+    this.hubConnection.onclose(() => {
+      debugger;
+      setTimeout(() => {
+        debugger;
+        this.hubConnection.start().then(() => {
+          debugger;
+          console.log("connection started");
+        }).catch(err => console.log(err));
+      }, 5000);
+    });
+
+    this.hubConnection.on("clientMethodName", (data) => {
+      debugger;
+      console.log(data);
+    });
+
+    this.hubConnection.on("WelcomeMethodName", (data) => {
+      debugger;
+      console.log(data);
+      this.hubConnection.invoke("GetDataFromClient", "user id", data).catch(err => console.log(err));
+    });
   }
+
+  public stopConnection() {
+    this.hubConnection.start().then(() => {
+      console.log("stopped");
+    }).catch(err => console.log(err));
+  }
+
 
 }
